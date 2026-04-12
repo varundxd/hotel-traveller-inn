1a3502984b8d3b8e9331be34640331b388f922dc
Divyanshu Srivastava <87711681+varundxd@users.noreply.github.com>
2026-04-12 15:40:47 +0530

    change 3

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,7 +33,7 @@ app.get('/api/health', (req, res) => {
 
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
